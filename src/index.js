5adff2ba2731cf89aa9cd740f4be0b673faf9012
serhiibulynin <serhiibulynin007@gmail.com>
2021-07-06 17:08:23 +0300

    добавил проверку на пустой массив

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './sass/main.scss';
 
-import formTpl from "./tpl/form.hbs"
-import galleryTpl from "./tpl/gallery.hbs"
-import cardTpl from "./tpl/card.hbs"
+import formTpl from './tpl/form.hbs';
+import galleryTpl from './tpl/gallery.hbs';
+import cardTpl from './tpl/card.hbs';
 import ImageApiService from './service/api-service';
 import BtnService from './service/btn-service';
 
-import { error,  defaultModules } from '@pnotify/core';
+import { notice, error, defaultModules } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import * as PNotifyMobile from '@pnotify/mobile';
 import '@pnotify/mobile/dist/PNotifyMobile.css';
@@ -20,59 +20,69 @@ const loadMoreBtn = new BtnService({
   hidden: true,
 });
 
-const divRef = document.querySelector("#root");
-
+const divRef = document.querySelector('#root');
 
 divRef.insertAdjacentHTML('beforeend', formTpl());
 divRef.insertAdjacentHTML('beforeend', galleryTpl());
 
 const listRef = document.querySelector('.gallery');
 const formRef = document.querySelector('#search-form');
-const errorRef = document.querySelector('.error');
 
-loadMoreBtn.refs.button.addEventListener("click", onLoadMore);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
 formRef.addEventListener('submit', onSubmitImage);
 
 function onSubmitImage(e) {
-    e.preventDefault();
-    listRef.innerHTML = "";
-    imageApiService.query = e.currentTarget.elements.query.value;
-    imageApiService.resetPage();
-    loadMoreBtn.show();
-    showImage();
+  e.preventDefault();
+  listRef.innerHTML = '';
+  imageApiService.query = e.currentTarget.elements.query.value;
+  imageApiService.resetPage();
+  loadMoreBtn.show();
+  showImage(false);
 }
 
 function onLoadMore(e) {
-    loadMoreBtn.disable();
-    imageApiService.incrementPage();
-    showImage();
+  loadMoreBtn.disable();
+  imageApiService.incrementPage();
+  showImage();
 }
 
-function showImage() {
-    imageApiService.fetchImage().then((data) => {
-        console.log(data.hits.length)
-        console.log(data.hits)
-        if (!data.hits.length) {
-            error({
-                text: 'Images not founded!'
-            })
-            listRef.innerHTML = "";
-            loadMoreBtn.hide();
-            return;
-        }
-        renderImage(data)
+function showImage(isLoadMore = true) {
+  imageApiService.fetchImage().then(data => {
+    if (data.hits.length) {
+      renderImage(data);
+      return;
+    }
+    loadMoreBtn.hide();
+    if (isLoadMore) {
+      notice({
+        text: 'This is the last page',
+      });
+
+      return;
+    }
+    error({
+      text: 'Images not found!',
     });
+  });
 }
-
-
 
 function renderImage(data) {
-    console.log(data.length)
-    listRef.insertAdjacentHTML('beforeend', cardTpl(data));
-    loadMoreBtn.show();
-    loadMoreBtn.enable();
-    listRef.scrollIntoView({
-        behavior: 'smooth',
-        block: 'end',
-    });
+  console.log(data.length);
+  listRef.insertAdjacentHTML('beforeend', cardTpl(data));
+  loadMoreBtn.show();
+  loadMoreBtn.enable();
+  listRef.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
 }
+
+// const myNotice = notice({
+//   text: "I'm a notice.",
+//   modules: new Map([
+//     ...defaultModules,
+//     [PNotifyPaginate, {
+//       // Paginate Module Options
+//     }]
+//   ])
+// });
